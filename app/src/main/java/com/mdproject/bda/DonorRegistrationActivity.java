be8f75a83b139cd832586cda554e008200ebeebf
Dharmik Patel <86761692+Dharmik0@users.noreply.github.com>
2022-10-31 19:17:32 +0530

    video4

--- app/src/main/java/com/mdproject/bda/DonorRegistrationActivity.java
+++ app/src/main/java/com/mdproject/bda/DonorRegistrationActivity.java
@@ -5,21 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
-    private Button registerButton;
-
+public class DonorRegistrationActivity extends AppCompatActivity {
+    private TextView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_donor_registration);
 
-        registerButton = findViewById(R.id.registerButton);
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,SelectRegistrationActivity.class);
+                Intent intent = new Intent(DonorRegistrationActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
