db5524ad814a39add0191266bac74a6254dfa195
Dharmik Patel <86761692+Dharmik0@users.noreply.github.com>
2022-11-02 23:59:30 +0530

    Firebase

--- app/src/main/java/com/mdproject/bda/LoginActivity.java
+++ app/src/main/java/com/mdproject/bda/LoginActivity.java
@@ -6,17 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button registerButton;
+    private TextView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        registerButton = findViewById(R.id.registerButton);
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,SelectRegistrationActivity.class);
